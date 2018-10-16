fd3b0d7db2b5a961e449db476ef98f2bc323f620
zhuangzeqin <zzq@eeepay.cn>
2018-10-16 15:13:51 +0800

    增加对象池类工具类

--- app/src/main/java/baseproject/demo/zzq/cn/eeepay/com/baseproject/mvp/ui/fragment/TestMVPFragment.java
+++ app/src/main/java/baseproject/demo/zzq/cn/eeepay/com/baseproject/mvp/ui/fragment/TestMVPFragment.java
@@ -1,9 +1,11 @@
 package baseproject.demo.zzq.cn.eeepay.com.baseproject.mvp.ui.fragment;
 
 import android.support.annotation.UiThread;
+import android.support.constraint.ConstraintLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 
@@ -37,11 +39,16 @@ public class TestMVPFragment extends BaseMvpFragment implements LoginView, Regis
     @PresenterVariable
     private RegisterPresenter mRegisterPresenter;
     @BindView(R.id.btn_login)
-     Button btn_Login;
+    Button btn_Login;
     @BindView(R.id.btn_register)
-     Button btn_Register;
+    Button btn_Register;
+    @BindView(R.id.btn_tes)
+    Button btnTes;
+    @BindView(R.id.clroot)
+    ConstraintLayout clRoot;
+    @BindView(R.id.ll_mRootView)
+    LinearLayout llRootView;
     int i = 0;
-
     @UiThread
     @Override
     public void loginSuccess(String msg) {
@@ -56,17 +63,18 @@ public class TestMVPFragment extends BaseMvpFragment implements LoginView, Regis
         ToastUtils.showLong("注册成功....");
     }
 
-    @OnClick({R.id.btn_login, R.id.btn_register})
+    @OnClick({R.id.btn_login, R.id.btn_register,R.id.btn_tes})
     public void onclickView(View view) {
         switch (view.getId()) {
             case R.id.btn_login:
                 //所有子View
                 mLoginPresenter2.login("13424230742", "123456q");//调用登录请求
 //                mLoginPresenter.login("13424230742", "123456q");//调用登录请求
-
                 break;
             case R.id.btn_register:
                 mRegisterPresenter.register();//调用注册请求
+                break;
+            case R.id.btn_tes:
                 break;
         }
     }
@@ -88,18 +96,26 @@ public class TestMVPFragment extends BaseMvpFragment implements LoginView, Regis
 
     @Override
     protected void init() {
-//        getViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                mLoginPresenter.login("13424230742", "123456q");//调用登录请求
-//            }
-//        });
-//
-//        getViewById(R.id.btn_register).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                mRegisterPresenter.register();//调用注册请求
-//            }
-//        });
+
+//        View custom = LayoutInflater.from(mContext)
+//                .inflate(R.layout.layout_custom, null);
+//        ((ImageView) (custom.findViewById(R.id.iv_custom))).setImageResource(R.mipmap.agency_nor);
+//        ((TextView) custom.findViewById(R.id.tv_custom_content)).setText("This is PageLayout");
+//        mPageLayout = new PageLayout.Builder(mContext)
+//                .initPage(clRoot)
+//                .setLoading(R.layout.layout_loading_demo, R.id.tv_page_loading_demo)
+//                .setEmpty(R.layout.layout_empty, R.id.tv_page_empty)
+//                .setCustomView(custom)
+//                .setError(R.layout.layout_error, R.id.tv_page_error_retry, new PageLayout.OnRetryClickListener() {
+//                    @Override
+//                    public void onRetry() {
+//                        ToastUtils.showShort("重试加载");
+//                        mPageLayout.showEmpty();
+//                    }
+//                })
+//                .setEmptyDrawable(R.drawable.pic_empty)
+//                .setErrorDrawable(R.drawable.pic_error)
+//                .setLoadingText("玩命加载中....")
+//                .create();
     }
 }
